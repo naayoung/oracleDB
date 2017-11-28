d210d0f18c1a83868f75a0e3765800aea67b12e1
groovypark <soobin950@gmail.com>
2017-11-29 00:34:02 +0900

    Update order.php

--- order.php
+++ order.php
@@ -2,30 +2,27 @@
 <!DOCTYPE html>
 <html lang="ko">
   <head>
-
     <style media="screen">
-  .container h2{
-    margin-left: 30px;
-  }
-  .customer_table,.delivery_address,.product_table{
-    border-top: 2px solid #cecece;
-margin-left: 30px;
-border-collapse: collapse;
-width: 100%;
-
-  }
-  table td.col2{
-    border-bottom: 1px solid #e4e4e4;
-    padding: 10px 16px;
-    }
-    table td.col1{
-    width: 104px;
-    border: solid #e4e4e4;
-    border-spacing: 0;
-    border-width: 0 1px 1px 0;
-    padding: 7px 10px 7px 15px;
-    background: #f4f4f4;
-    }
+      .order {
+        margin-top: 8em;
+      }
+      .customer_table,.delivery_address,.product_table{
+        border-top: 2px solid #cecece;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      table td.col2{
+        border-bottom: 1px solid #e4e4e4;
+        padding: 10px 16px;
+      }
+      table td.col1{
+        width: 104px;
+        border: solid #e4e4e4;
+        border-spacing: 0;
+        border-width: 0 1px 1px 0;
+        padding: 7px 10px 7px 15px;
+        background: #f4f4f4;
+      }
     </style>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -35,80 +32,56 @@ width: 100%;
     <link rel="icon" href="./favicon.png">
 
     <title>Delivery Management System</title>
-
-    <!-- Bootstrap CSS -->
+    
+    <!-- CSS -->
+    <link rel="icon" href="./favicon.png">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href="css/carousel.css" rel="stylesheet">
+
     <!-- Bootstrap Javascript -->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="http://googledrive.com/host/0B-QKv6rUoIcGeHd6VV9JczlHUjg"></script>
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="js/ie-emulation-modes-warning.js"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Custom styles for this template -->
   </head>
 <!-- NAVBAR
 ================================================== -->
   <body>
-    <div class="navbar-wrapper">
-      <div class="container">
-        <nav class="navbar navbar-inverse navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">택배관리시스템</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
+        <div class="navbar-wrapper">
+            <div class="container">
+                <nav class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img height="30em" src="./favicon.png"></img> D.M.S.</a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.html">Home</a></li>
+                        <li class="active"><a href="order.html">Order</a></li>
+                        <li><a href="order_check.html">Check</a></li>
+                        <li><a href="order_change.html">Change</a></li>                
                     </ul>
-                </li>
-              </ul>
+                    </div>
+                </div>
+                </nav>
             </div>
-          </div>
-        </nav>
-      </div>
-    </div>
-/
+        </div>
+
     <!-- START THE FEATURETTES -->
     <div class="container marketing">
       <form class="order" action="http://localhost/order_finish.php" method="post">
-<p> <h1>주문</h1> </p>
-
-
-<p> <h2>구매자정보</h2> </p>
-<p></p>
-<table class="customer_table">
-
-
-
-
- <tr>
+        <p> <h1>주문</h1> </p>
+        <p> <h2>구매자정보</h2> </p>
+        <p></p>
+        <table class="customer_table">
+            <tr>
         <td class="col1">  구매자ID </td>
         <td class="col2"><input type="text" name="ID" value="" placeholder="ID"></td>
  </tr>
@@ -186,9 +159,9 @@ width: 100%;
         <hr class="featurette-divider">
 
         <!-- FOOTER -->
-        <footer>
-            <p class="pull-right"><a href="#">Back to top</a></p>
-            <p>&copy; 2017 Delivery Management System, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      <footer>
+          <p class="pull-right"><a href="#">Back to top</a></p>
+          <p>&copy; 2017 Delivery Management System &middot; <a href="#">Privacy</a></p>
         </footer>
     </div><!-- /.container -->
 
